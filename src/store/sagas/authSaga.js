b6f329776978bdd7652edf0f7c64be08f72d8ebd
talaxasi <kiriesk2@mail.ru>
2021-08-31 15:58:12 +0300

    changed authSagaWorker

--- src/store/sagas/authSaga.js
+++ src/store/sagas/authSaga.js
@@ -1,16 +1,16 @@
 import {put, takeLatest} from 'redux-saga/effects';
-import axios from "axios";
 import {AUTH_REQUEST} from "../types";
 import {authFailure, authSuccess} from "../actions";
 
 function* authSagaWorker (action) {
   try {
-    const response = yield axios.post('http://localhost:3001/users', {
-      login: action.payload.login,
-      password: action.payload.password
-    })
-    const ok = yield response.statusText === 'Created';
-    if (ok) yield put(authSuccess(action.payload))
+    // const response = yield axios.post('http://localhost:3001/users', {
+    //   login: action.payload.login,
+    //   password: action.payload.password
+    // })
+    // const ok = yield response.statusText === 'Created';
+    // if (ok)
+      yield put(authSuccess(action.payload))
   }
   catch (e) {
     console.log(e);
